d8b152390cbee88e614c36fec8b765095a8325f0
mmiddlezong <119022671+mmiddlezong@users.noreply.github.com>
2024-12-13 11:28:54 -0500

    Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,71 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [nIn, setNIn] = useState<string>("");
+    const [pIn, setPIn] = useState<string>("");
+    const [mLIn, setMLIn] = useState<string>("");
+    const [mSIn, setMSIn] = useState<string>("any");
+    const [message, setMessage] = useState<string>("");
+    async function handleSubmit(e: any) {
+        e.preventDefault();
+        let count: number = 0;
+        for (let n = 1; n < 100; n++) {
+            for (let p = 0; p < n; p++) {
+                for (let mL = 0 - p; mL <= p; mL++) {
+                    ["+1/2", "-1/2"].forEach((mS) => {
+                        const nMatch = nIn.length == 0 || n == parseInt(nIn);
+                        const pMatch = pIn.length == 0 || p == parseInt(pIn);
+                        const mLMatch = mLIn.length == 0 || mL == parseInt(mLIn);
+                        const mSMatch = mSIn === "any" || mS === mSIn;
+                        if (nMatch && pMatch && mLMatch && mSMatch) {
+                            count++;
+                        }
+                    });
+                }
+            }
+        }
+        setMessage(`Total # of possible electrons: ${count}`)
+    }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+        <div className="container mx-auto px-4 py-2">
+            <h1 className="text-2xl mb-4">Chemistry Electron Counter</h1>
+            <div className="mb-2">
+                <label>
+                    n:
+                    <input type="number" className="mx-2 bg-slate-200" name="n" value={nIn} onChange={(e) => setNIn(e.target.value)} />
+                </label>
+            </div>
+            <div className="mb-2">
+                <label>
+                    p:
+                    <input type="number" className="mx-2 bg-slate-200" name="p" value={pIn} onChange={(e) => setPIn(e.target.value)} />
+                </label>
+            </div>
+            <div className="mb-2">
+                <label>
+                    m_l:
+                    <input type="number" className="mx-2 bg-slate-200" name="m_l" value={mLIn} onChange={(e) => setMLIn(e.target.value)} />
+                </label>
+            </div>
+            <div className="mb-2">
+                <label>
+                    m_s:
+                    <select name="m_s" className="mx-2 bg-slate-200 px-2 py-1" value={mSIn} onChange={(e) => setMSIn(e.target.value)}>
+                        <option value="any">any</option>
+                        <option value="+1/2">+1/2</option>
+                        <option value="-1/2">-1/2</option>
+                    </select>
+                </label>
+            </div>
+            <button className="bg-slate-200 px-4 py-2 rounded" onClick={(e) => handleSubmit(e)}>
+                <div>Calculate</div>
+            </button>
+            <div>
+              {message}
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
